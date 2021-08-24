3bd0c6d4766a3fafece186422cbb66c62bcf70ce
Mateus Soares <mateus123soares@hotmail.com>
2021-08-24 19:02:32 -0300

    Adicionando testes

--- test/create_event.spec.js
+++ test/create_event.spec.js
@@ -20,7 +20,38 @@ describe('Event', () => {
 
         const responseEvent = await request(app)
             .get('/eventos/')
-            
+
         expect(responseEvent.body[0]).toHaveProperty('_id');
+    })
+
+    it('should be able to create a edit event', async () => {
+
+        const responseEventGet = await request(app)
+        .get('/eventos/')
+
+        const id = responseEventGet.body[0]._id
+
+        const responseEvent = await request(app)
+            .put(`/eventos/editar/${id}`)
+            .send({
+                nome: "APAD22",
+                descricao: "test@hotma.com",
+                local: "0000000000",
+            });
+
+        expect(responseEvent.body.nome).toEqual('APAD22');
+    })
+
+    it('should be able to create a delete event', async () => {
+
+        const responseEventGet = await request(app)
+        .get('/eventos/')
+
+        const id = responseEventGet.body[0]._id
+
+        const responseEvent = await request(app)
+            .delete(`/eventos/delete/${id}`)
+        
+        expect(responseEvent.body._id).toEqual(id);
     })
 })
